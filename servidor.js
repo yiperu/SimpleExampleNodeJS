ac5b7c46ca35596768a878bbe61bd9dc65650b2b
Henry AT <yiperu@gmail.com>
2016-01-24 11:42:15 -0300

    Add support for save registre of visit pages

--- servidor.js
+++ servidor.js
@@ -1,5 +1,6 @@
 var servidor = require('http');
 var url = require('url');
+var fs = require('fs');
 
 function iniciar(enrutar,manejador){
 	function arrancaServidor(require, respuesta) {
@@ -7,6 +8,10 @@ function iniciar(enrutar,manejador){
 		var ruta = url.parse(require.url).pathname;
 		console.log("Alguien se ha conectado.")
 		var contenido = enrutar(manejador,ruta);
+
+		// Aqui registrare las rutas visitadas
+		var registro = fs.createWriteStream('registro.txt',{'flags':'a'});
+		registro.write(ruta + '\n');
 
 		respuesta.writeHead(200,{"Content-Type":"text/html"});
 		respuesta.write(contenido);
