11cc63a5e7b1d5dab7d136908a2505c6e02b637d
Henry AT <yiperu@gmail.com>
2016-01-24 11:49:53 -0300

    Put a little example for see block code

--- peticiones.js
+++ peticiones.js
@@ -1,5 +1,10 @@
 function inicio(){
 	console.log("Has entrado en la pagina inicio");
+
+	// Agregamos aqui un delay de 10 segundos para ver el codigo bloqueante de NodeJS
+	// Ya que este ses ejecuta en un solo hilo
+	var ahora = new Date().getTime();
+	while(new Date().getTime() < ahora + 10000);
 	return "Inicio";
 }
 
